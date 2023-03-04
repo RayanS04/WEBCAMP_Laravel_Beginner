a4ba67efdb690a101cd3624ddb1b53ba0324617d
Rayan <@example.com>
2023-03-04 12:23:02 +0000

    認証と認可の実装まで書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -10,8 +10,8 @@
                 タスク名:<input><br>
                 期限:<input type="date"><br>
                 タスク詳細:<textarea></textarea><br>
-                重要度:<label><input type="radio" name="priority">低い</label> / 
-                    <label><input type="radio" name="priority" checked>普通</label> / 
+                重要度:<label><input type="radio" name="priority">低い</label> /
+                    <label><input type="radio" name="priority" checked>普通</label> /
                     <label><input type="radio" name="priority">高い</label><br>
                 <button>タスクを登録する</button>
             </form>
@@ -54,12 +54,12 @@
         </table>
         <!-- ページネーション -->
         現在 1 ページ目<br>
-        <a href="./top.html">最初のページ(未実装)</a> / 
-        <a href="./top.html">前に戻る(未実装)</a> / 
+        <a href="./top.html">最初のページ(未実装)</a> /
+        <a href="./top.html">前に戻る(未実装)</a> /
         <a href="./top.html">次に進む(未実装)</a>
         <br>
         <hr>
         <menu label="リンク">
-        <a href="./index.html">ログアウト(未実装)</a><br>
+        <a href="/logout">ログアウト</a><br>
         </menu>
 @endsection
